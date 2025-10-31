d3fa983ae10b3d5512d50154f5b5579d9a3dffda
YarmyCode <johnayarmahe@gmail.com>
2025-10-31 23:36:16 +0000

    fix(mail): Resolve queue error

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('queue:work')->everyMinute()->withoutOverlapping();
+Schedule::command('queue:listen')->everyMinute()->withoutOverlapping();
